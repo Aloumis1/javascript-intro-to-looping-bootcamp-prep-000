b4deb4ac70c3fccd1546dd99e0ddcc536c71c1bf
Achilles P. Loumis <achilleas.loumis@gmail.com>
2018-11-15 02:45:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function doWhileLoop() {
   return = 'I am ${i} strange loop' 
 }
 
-unction forLoop(array) {
+function forLoop(array) {
    for ( let i = 0; i < 25; i++) {
      if (i === 1) {
        array.push(`I am 1 strange loop.`)
