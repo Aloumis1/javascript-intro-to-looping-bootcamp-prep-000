fb9bd9eb6b97a950a3d0e8a470bb6e8b9e334366
Achilles P. Loumis <achilleas.loumis@gmail.com>
2018-11-15 02:46:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,10 @@
-function doWhileLoop() {
-  return = 'I am ${i} strange loop' 
-}
-
-function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+function forLoop(array){
+  for(var i = 0; i <25; i++){
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
   }
-  return array;
+}
+return array
 }
